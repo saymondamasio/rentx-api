75145fbe1aab38ffa98b66ed0d3ca3965fb758bf
Saymon Damásio <saymon.damasio95@gmail.com>
2021-11-06 09:14:17 -0300

    salvando categorias do arquivo no banco

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -3,18 +3,55 @@ import fs from 'fs'
 
 import { CategoriesRepository } from '../../repositories/implementations/CategoriesRepository'
 
+interface IImportCategory {
+  name: string
+  description: string
+}
+
 export class ImportCategoryUseCase {
   constructor(private categoriesRepository: CategoriesRepository) {}
 
-  execute(file: Express.Multer.File): void {
-    const stream = fs.createReadStream(file.path)
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const categories: IImportCategory[] = []
 
-    const parseFile = csvParse()
+      const stream = fs.createReadStream(file.path)
 
-    stream.pipe(parseFile)
+      const parseFile = csvParse()
 
-    stream.on('data', async line => {
-      console.log(line)
+      stream.pipe(parseFile)
+
+      parseFile
+        .on('data', async line => {
+          const [name, description] = line
+
+          categories.push({
+            name,
+            description,
+          })
+        })
+        .on('end', () => {
+          resolve(categories)
+        })
+        .on('error', (err: Error) => {
+          reject(err)
+        })
+    })
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file)
+
+    categories.map(async category => {
+      const { name, description } = category
+
+      const categoryExists = this.categoriesRepository.findByName(name)
+      if (!categoryExists) {
+        this.categoriesRepository.create({
+          name,
+          description,
+        })
+      }
     })
   }
 }
