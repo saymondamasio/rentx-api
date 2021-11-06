eaa5bef659c873226be99dd78cbe1744a0761e81
Saymon Damásio <saymon.damasio95@gmail.com>
2021-11-06 10:18:47 -0300

    adicionando a documentação da API com swagger

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from 'express'
+import swaggerUI from 'swagger-ui-express'
 
+import swaggerConfig from '../swagger.json'
 import { routes } from './routes'
 
 const app = express()
 
 app.use(express.json())
+
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerConfig))
 
 app.use(routes)
 
