a3d877a1f66d5fbaa0d325692319ea0ba6b8c4ad
Saymon Damásio <saymon.damasio95@gmail.com>
2021-11-06 08:39:00 -0300

    lendo arquivo receibido por partes

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,9 +1,20 @@
+import csvParse from 'csv-parse'
+import fs from 'fs'
+
 import { CategoriesRepository } from '../../repositories/implementations/CategoriesRepository'
 
 export class ImportCategoryUseCase {
   constructor(private categoriesRepository: CategoriesRepository) {}
 
   execute(file: Express.Multer.File): void {
-    console.log(file)
+    const stream = fs.createReadStream(file.path)
+
+    const parseFile = csvParse()
+
+    stream.pipe(parseFile)
+
+    stream.on('data', async line => {
+      console.log(line)
+    })
   }
 }
