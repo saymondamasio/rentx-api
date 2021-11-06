41bf31619e901fad6dab1bd948a1ce55361502fc
Saymon Damásio <saymon.damasio95@gmail.com>
2021-11-06 16:37:28 -0300

    criando a migração de criação de categorias

--- src/modules/cars/entities/Category.ts
+++ src/modules/cars/entities/Category.ts
@@ -1,6 +1,25 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm'
+
+@Entity('categories')
 export class Category {
+  @PrimaryGeneratedColumn('uuid')
   id: string
+
+  @Column()
   name: string
+
+  @Column()
   description: string
+
+  @CreateDateColumn()
   created_at: Date
+
+  @UpdateDateColumn()
+  updated_at: Date
 }
