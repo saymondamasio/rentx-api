f24c8786e48c1d896494c301e6cfabfb7bc98715
Saymon Damásio <saymon.damasio95@gmail.com>
2021-11-05 22:01:29 -0300

    reafatorando repositorios para Singleton Pattern

--- src/modules/cars/repositories/SpecificationsRepository.ts
+++ src/modules/cars/repositories/SpecificationsRepository.ts
@@ -7,8 +7,18 @@ import { ISpecificationsRepository } from './ISpecificationsRepository'
 export class SpecificationsRepository implements ISpecificationsRepository {
   private specifications: Specification[]
 
-  constructor() {
+  private static INSTANCE: SpecificationsRepository
+
+  private constructor() {
     this.specifications = []
+  }
+
+  public static getInstance(): SpecificationsRepository {
+    if (!SpecificationsRepository.INSTANCE) {
+      SpecificationsRepository.INSTANCE = new SpecificationsRepository()
+    }
+
+    return SpecificationsRepository.INSTANCE
   }
 
   findByName(name: string): Specification {
