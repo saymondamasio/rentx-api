f90cbe6dc2ca3b1df06613917b1692d572cbb105
Saymon Damásio <saymon.damasio95@gmail.com>
2022-01-17 13:55:17 -0300

    refactor: configure typeorm as per environment

---
+++ ormconfig.js
@@ -0,0 +1,78 @@
+require('dotenv/config')
+
+const devConfig = [
+  {
+    name: 'default',
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    uuidExtension: 'pgcrypto',
+    cache: true,
+    ssl: true,
+    extra: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+
+    migrations: ['./src/shared/infra/typeorm/migrations/*.ts'],
+    entities: ['./src/modules/**/entities/*.ts'],
+    cli: {
+      migrationsDir: './src/shared/infra/typeorm/migrations',
+      entitiesDir: './src/modules/**/entities',
+    },
+  },
+  {
+    name: 'mongo',
+    type: 'mongodb',
+    host: process.env.MONGO_HOST,
+    port: process.env.MONGO_PORT,
+    database: process.env.MONGO_NAME,
+    username: process.env.MONGO_USER,
+    password: process.env.MONGO_PASS,
+    useUnifiedTopology: true,
+    entities: ['./src/modules/**/infra/typeorm/schemas/*.ts'],
+  },
+]
+
+const prodConfig = [
+  {
+    name: 'default',
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    uuidExtension: 'pgcrypto',
+    cache: true,
+    ssl: true,
+    extra: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+
+    entities: ['./dist/modules/**/infra/typeorm/entities/*.js'],
+    migrations: ['./dist/shared/infra/typeorm/migrations/*.js'],
+    cli: {
+      migrationsDir: './dist/shared/infra/typeorm/migrations',
+    },
+  },
+  {
+    name: 'mongo',
+    type: 'mongodb',
+    host: process.env.MONGO_HOST,
+    port: process.env.MONGO_PORT,
+    database: process.env.MONGO_NAME,
+    username: process.env.MONGO_USER,
+    password: process.env.MONGO_PASS,
+    useUnifiedTopology: true,
+    entities: ['./dist/modules/**/infra/typeorm/schemas/*.js'],
+  },
+]
+
+module.exports = process.env.NODE_ENV === 'development' ? devConfig : prodConfig
