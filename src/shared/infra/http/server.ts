7702ba02d212f9b574f1229b17ecbceb3a97b08c
Saymon Damásio <saymon.damasio95@gmail.com>
2022-01-17 13:42:33 -0300

    feat: rename APP_PORT

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata'
 import { app } from './app'
 
-app.listen(process.env.APP_PORT || 3333, () => {
-  console.log(`Server running on port ${process.env.APP_PORT || 3333}`)
+app.listen(process.env.PORT || 3333, () => {
+  console.log(`Server running on port ${process.env.PORT || 3333}`)
 })
