80ae74c5b54e6a1629a774e56ba6ce8eae350d74
Saymon Damásio <saymon.damasio95@gmail.com>
2022-01-17 15:43:37 -0300

    chore: update rate-limiter-flexible

--- src/shared/infra/http/middlewares/rateLimiter.ts
+++ src/shared/infra/http/middlewares/rateLimiter.ts
@@ -5,36 +5,47 @@ import { createClient } from 'redis'
 import { cacheConfig } from '@config/cache'
 import { AppError } from '@shared/errors/AppError'
 
-const redisClient = createClient({
-  url: `redis://${cacheConfig.config.redis.host}:${cacheConfig.config.redis.port}`,
-  password: cacheConfig.config.redis.password,
-  socket: {
-    tls: true,
-    rejectUnauthorized: false,
-  },
-})
-
-const limiter = new RateLimiterRedis({
-  storeClient: redisClient,
-  keyPrefix: 'rateLimiter',
-  points: 5,
-  duration: 1,
-})
-
-redisClient.on('error', function (err) {
-  console.log('Error ' + err)
-})
-
 export async function rateLimiter(
   req: Request,
   res: Response,
   next: NextFunction
 ): Promise<void> {
+  const redisClient = createClient({
+    legacyMode: true,
+    url: `redis://${cacheConfig.config.redis.host}:${cacheConfig.config.redis.port}`,
+    password: cacheConfig.config.redis.password,
+    socket: {
+      host: cacheConfig.config.redis.host,
+      port: cacheConfig.config.redis.port,
+      tls: true,
+      rejectUnauthorized: false,
+    },
+  })
+
+  await redisClient.connect()
+
+  redisClient.on('error', err => console.log(`${err.name}: ${err.message}`))
+
+  const limiter = new RateLimiterRedis({
+    storeClient: redisClient,
+    keyPrefix: 'rate-limit',
+    points: 20,
+    duration: 5,
+    blockDuration: 10,
+  })
   try {
     await limiter.consume(req.ip)
 
     return next()
-  } catch {
+  } catch (rateLimiterRes) {
+    req.headers['Retry-After'] = String(rateLimiterRes.msBeforeNext / 1000)
+    req.headers['X-RateLimit-Limit'] = String(20)
+    req.headers['X-RateLimit-Remaining'] = String(
+      rateLimiterRes.remainingPoints
+    )
+    req.headers['X-RateLimit-Reset'] = new Date(
+      Date.now() + rateLimiterRes.msBeforeNext
+    ).toString()
     throw new AppError('Too many requests', 429)
   }
 }
