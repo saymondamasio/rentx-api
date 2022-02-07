fe91be1f0419f4708d911d6feb304fe705c2ca74
Saymon Damásio <saymon.damasio95@gmail.com>
2022-02-07 16:21:23 -0300

    refactor: rate limiter

--- src/shared/infra/http/middlewares/rateLimiter.ts
+++ src/shared/infra/http/middlewares/rateLimiter.ts
@@ -5,27 +5,27 @@ import { createClient } from 'redis'
 import { cacheConfig } from '@config/cache'
 import { AppError } from '@shared/errors/AppError'
 
+const redisClient = createClient({
+  legacyMode: true,
+  url: cacheConfig.config.redis.url,
+  socket: {
+    tls: true,
+    rejectUnauthorized: false,
+  },
+})
+
+const limiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: 'rateLimiter',
+  points: 10, // 10 requests
+  duration: 1, // por 1 second, by IP
+})
+
 export async function rateLimiter(
   request: Request,
   response: Response,
   next: NextFunction
 ): Promise<void> {
-  const redisClient = createClient({
-    legacyMode: true,
-    url: cacheConfig.config.redis.url,
-    socket: {
-      tls: true,
-      rejectUnauthorized: false,
-    },
-  })
-
-  const limiter = new RateLimiterRedis({
-    storeClient: redisClient,
-    keyPrefix: 'rateLimiter',
-    points: 10, // 10 requests
-    duration: 1, // por 1 second, by IP
-  })
-
   try {
     await redisClient.connect()
 
